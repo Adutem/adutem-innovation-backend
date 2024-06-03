de06720cec3372b3ea0ee26771ea8a4f88e97c87
philipthedeveloper <88097351+philipthedeveloper@users.noreply.github.com>
2024-06-03 13:49:03 +0100

    SecurityFix: Firebase config data Exposed

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { config } from "dotenv";
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1K8-D48MWdvjQNgLl742bCzxKDzz5zIo",
-  authDomain: "acorn-auto.firebaseapp.com",
-  projectId: "acorn-auto",
-  storageBucket: "acorn-auto.appspot.com",
-  messagingSenderId: "1080404539190",
-  appId: "1:1080404539190:web:fd739ba57f39310cc9a597",
-  measurementId: "G-WF1H0MMZ7R",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
